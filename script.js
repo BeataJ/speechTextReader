70abe172679130e9607fdf4f3d7b64b5c487817a
beata <beata@emc2wave.com>
2020-06-03 18:17:08 -0700

    add event listener to readBtn

--- script.js
+++ script.js
@@ -133,4 +133,10 @@ closeBtn.addEventListener('click', () =>
 // Change voice
 voiceSelect.addEventListener('change', setVoice);
 
+// Read text button
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
 getVoices();
